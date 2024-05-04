8cb1adf5a69f36b53ee08964548521238cc6f643
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-04 20:00:03 +0300

    userslist 1

--- controller-models/UserList.php
+++ controller-models/UserList.php
@@ -7,7 +7,24 @@ abstract class UserList {
 }
 class FollowingList extends UserList {
     public User $user;
-    function gatherList(mysqli $db){}
+    function gatherList(mysqli $db){
+        $stmt = $db->prepare("SELECT * FROM Users JOIN Follows ON Follows.Followed = Users.Poster WHERE Follows.Followed = ?");
+        $stmt->bind_param('i', $this->user->UUID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if (!$result) {
+            echo "errooor: " . $db->error . "\n";
+        }
+        $users = [];
+        while ($row = $result->fetch_assoc()) {
+            print_r($row);
+            $user = User::CreateFromArr($row);
+            print_r($user);
+            $users[] = $user;
+        }
+        $this->users = $users;
+        
+    }
 }
 class FollowerList extends UserList {
     public User $user;
