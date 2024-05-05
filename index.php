e1a8f411a87d25d2d44283d4ab4630521f54108c
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-05 22:34:19 +0300

    post rendering

--- index.php
+++ index.php
@@ -4,17 +4,18 @@ require_once "./views/head.php";
 
 <main>
     <h1>Hello, world!</h1>
-    <div class="card tweet">
-        <div class="card-body">
-            <h5 class="card-title"><img src="images/pexels-pixabay-45201.jpg" class="rounded-circle pfp" alt="..."> Tweeter</h5>
-            <p class="card-text tweet-username">@goog</p>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <button class="btn btn-link card-link">like</button>
-            <button class="btn btn-link card-link">repost</button>
-            <a href="#" class="card-link">comment</a>
-        </div>
-        <img src="images/pexels-mikebirdy-170811.jpg" class="card-img-bottom" alt="...">
-    </div>
+    <?php
+    $post = new Post;
+    $post->post_id=0;
+    $post->poster=new User;
+    $post->poster->username="goog";
+    $post->poster->display_name="Tweeter";
+    $post->poster->profile_pic="images/pexels-pixabay-45201.jpg";
+    $post->content="Some quick example text to build on the card title and make up the bulk of the card's content.";
+    $post->date=new DateTime();
+    $post->image="images/pexels-mikebirdy-170811.jpg";
+     require_once "./views/post-small.php"; 
+     ?>
 </main>
 <?php
 require_once "./views/foot.php";
---
+++ views/post-small.php
@@ -0,0 +1,13 @@
+<div class="card tweet">
+    <div class="card-body">
+        <h5 class="card-title"><img src="<?=$post->poster->profile_pic?>" class="rounded-circle pfp" alt="..."> <?=$post->poster->display_name?></h5>
+        <p class="card-text tweet-username">@<?=$post->poster->username?></p>
+        <p class="card-text"><?=$post->content?></p>
+        <button class="btn btn-link card-link">like</button>
+        <button class="btn btn-link card-link">repost</button>
+        <a href="#" class="card-link">comment</a>
+    </div>
+    <?php if (!is_null($post->image)): ?>
+    <img src="<?=$post->image?>" class="card-img-bottom" alt="...">
+    <?php endif; ?>
+</div>
