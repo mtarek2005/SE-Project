a3dc5c294b527c68ac81d7866c176998d5694363
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-05 22:06:31 +0300

    style into file

--- index.php
+++ index.php
@@ -10,19 +10,13 @@ require_once "./include.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="scripts/jquery-3.7.1.min.js"></script>
-    <style>
-        main {
-            margin-left: auto;
-            margin-right: auto;
-            max-width: fit-content;
-        }
-    </style>
+    <link href="styles/main.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#" style="font-weight:bold; font-size:225%">ℤ</a>
+            <a class="navbar-brand" href="#">ℤ</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -60,10 +54,10 @@ require_once "./include.php";
     </nav>
     <h1>Hello, world!</h1>
     <main>
-        <div class="card" style="max-width: 36rem;">
+        <div class="card tweet">
             <div class="card-body">
-                <h5 class="card-title"><img src="images/pexels-pixabay-45201.jpg" class="rounded-circle" style="object-fit: cover; width: 3rem; height:3rem;" alt="..."> Tweeter</h5>
-                <p class="card-text" style="opacity:0.75;font-size:75%">@goog</p>
+                <h5 class="card-title"><img src="images/pexels-pixabay-45201.jpg" class="rounded-circle pfp" alt="..."> Tweeter</h5>
+                <p class="card-text tweet-username">@goog</p>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <button class="btn btn-link card-link">like</button>
                 <button class="btn btn-link card-link">repost</button>
