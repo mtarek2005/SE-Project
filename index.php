cc4049dafe87951487c98e246a127bfd84fd0090
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-05 21:14:10 +0300

    tweet

--- index.php
+++ index.php
@@ -52,6 +52,17 @@ require_once "./include.php";
         </div>
     </nav>
     <h1>Hello, world!</h1>
+    <main>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Tweeter</h5>
+                <p class="card-text" style="opacity:0.75;font-size:75%">@goog</p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <img src="images/pexels-mikebirdy-170811.jpg" class="card-img-bottom" alt="...">
+        </div>
+    </main>
 </body>
 
 </html>
