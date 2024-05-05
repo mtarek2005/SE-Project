0f53953e1ba5563c2d83a114db5b6637ac7d2edd
Marwan Zaki <marwanzaki1105@icloud.com>
2024-05-05 23:07:34 +0300

    new database schema

--- se-project-z.sql
+++ se-project-z.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 01, 2024 at 08:57 PM
+-- Generation Time: May 05, 2024 at 10:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,6 +33,13 @@ CREATE TABLE `Blocks` (
   `Blocked` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `Blocks`
+--
+
+INSERT INTO `Blocks` (`Blocker`, `Blocked`) VALUES
+(0, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,15 @@ CREATE TABLE `Bookmarks` (
   `Post` int(11) NOT NULL,
   `Date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Bookmarks`
+--
+
+INSERT INTO `Bookmarks` (`User`, `Post`, `Date`) VALUES
+(0, 1, '2024-05-04 00:01:48'),
+(0, 2, '2024-05-04 21:00:43'),
+(0, 4, '2024-05-04 21:00:33');
 
 -- --------------------------------------------------------
 
@@ -80,6 +96,14 @@ CREATE TABLE `Likes` (
   `Date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `Likes`
+--
+
+INSERT INTO `Likes` (`User`, `Post`, `Date`) VALUES
+(0, 2, '2024-05-04 20:11:51'),
+(1, 1, '2024-05-04 20:09:41');
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +115,13 @@ CREATE TABLE `Mutes` (
   `Muter` int(11) NOT NULL,
   `Muted` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Mutes`
+--
+
+INSERT INTO `Mutes` (`Muter`, `Muted`) VALUES
+(0, 0);
 
 -- --------------------------------------------------------
 
@@ -107,6 +138,14 @@ CREATE TABLE `PMs` (
   `Date` datetime NOT NULL DEFAULT current_timestamp(),
   `Reaction` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `PMs`
+--
+
+INSERT INTO `PMs` (`MessageID`, `From_user`, `To_user`, `Content`, `Date`, `Reaction`) VALUES
+(827757441, 1, 0, 'This is a test message', '2024-05-04 21:25:04', '💩'),
+(1161868969, 0, 1, 'This is a test reply to that message', '2024-05-04 21:25:04', NULL);
 
 -- --------------------------------------------------------
 
@@ -125,6 +164,18 @@ CREATE TABLE `Post` (
   `Image` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `Post`
+--
+
+INSERT INTO `Post` (`PostID`, `Poster`, `Post_replied_to`, `Post_type`, `Content`, `Post_date`, `Image`) VALUES
+(1, 0, NULL, 'main', 'This is a test post', '2024-05-03 21:01:46', NULL),
+(2, 0, 1, 'reply', 'This is a test reply.', '2024-05-03 22:26:22', NULL),
+(3, 0, NULL, 'main', 'This is a test post bla bla bla', '2024-05-04 20:42:42', 'images/pexels-mikebirdy-170811.jpg'),
+(4, 0, 1, 'quote', 'This is a test quote repost.', '2024-05-04 20:43:24', NULL),
+(5, 1, NULL, 'main', 'This is a test post', '2024-05-04 20:53:32', NULL),
+(6, 1, 1, 'quote', 'This is a test quote post ', '2024-05-04 20:54:00', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -137,6 +188,14 @@ CREATE TABLE `Reposts` (
   `Post` int(11) NOT NULL,
   `Date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Reposts`
+--
+
+INSERT INTO `Reposts` (`User`, `Post`, `Date`) VALUES
+(0, 1, '2024-05-03 22:36:33'),
+(1, 3, '2024-05-04 20:54:56');
 
 -- --------------------------------------------------------
 
@@ -151,7 +210,7 @@ CREATE TABLE `Users` (
   `Hashed_password` text NOT NULL,
   `Display_name` text DEFAULT '',
   `About` text DEFAULT '',
-  `Profile_pic` text DEFAULT NULL,
+  `Profile_pic` text DEFAULT 'images/profile_unkown.png',
   `Join_date` date NOT NULL DEFAULT current_timestamp(),
   `Mute_to` datetime DEFAULT NULL,
   `Ban_to` datetime DEFAULT NULL,
@@ -163,8 +222,8 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`UUID`, `Username`, `Hashed_password`, `Display_name`, `About`, `Profile_pic`, `Join_date`, `Mute_to`, `Ban_to`, `Role`) VALUES
-(0, 'marwanzaki', '482c811da5d5b4bc6d497ffa98491e38', 'Marwan Zaki', 'about me', NULL, '2024-05-01', NULL, NULL, 'regular'),
-(1, 'mtarek', '3193150bfbe7f1ffea91740b124ff236', 'Mohamed Tarek', '', NULL, '2024-05-01', NULL, NULL, 'regular');
+(0, 'marwanzaki', '482c811da5d5b4bc6d497ffa98491e38', 'Marwan Zaki', 'about me', 'images/profile_unkown.png', '2024-05-01', NULL, NULL, 'regular'),
+(1, 'mtarek', '3193150bfbe7f1ffea91740b124ff236', 'Mohamed Tarek', '', 'images/profile_unkown.png', '2024-05-01', NULL, NULL, 'regular');
 
 --
 -- Indexes for dumped tables
@@ -181,7 +240,8 @@ ALTER TABLE `Blocks`
 -- Indexes for table `Bookmarks`
 --
 ALTER TABLE `Bookmarks`
-  ADD PRIMARY KEY (`User`,`Post`);
+  ADD PRIMARY KEY (`User`,`Post`),
+  ADD KEY `post_bookmark` (`Post`);
 
 --
 -- Indexes for table `Follows`
@@ -246,6 +306,13 @@ ALTER TABLE `Blocks`
   ADD CONSTRAINT `blocker` FOREIGN KEY (`Blocker`) REFERENCES `Users` (`UUID`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `Bookmarks`
+--
+ALTER TABLE `Bookmarks`
+  ADD CONSTRAINT `post_bookmark` FOREIGN KEY (`Post`) REFERENCES `Post` (`PostID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_bookmark` FOREIGN KEY (`User`) REFERENCES `Users` (`UUID`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `Follows`
 --
 ALTER TABLE `Follows`
@@ -263,7 +330,8 @@ ALTER TABLE `Likes`
 -- Constraints for table `Mutes`
 --
 ALTER TABLE `Mutes`
-  ADD CONSTRAINT `muted` FOREIGN KEY (`Muted`) REFERENCES `Users` (`UUID`) ON DELETE CASCADE;
+  ADD CONSTRAINT `muted` FOREIGN KEY (`Muted`) REFERENCES `Users` (`UUID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `muter` FOREIGN KEY (`Muter`) REFERENCES `Users` (`UUID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `PMs`
