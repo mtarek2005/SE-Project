8e8720060b02a5d46a5e76127d2c772f20b25af7
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-05 23:42:25 +0300

    favicons

--- views/head.php
+++ views/head.php
@@ -11,19 +11,20 @@ require_once "./include.php";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="scripts/jquery-3.7.1.min.js"></script>
     <link href="styles/main.css" rel="stylesheet">
+    <link rel="icon" href="./images/favicon.ico">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">ℤ</a>
+            <a class="navbar-brand" href="index.php">ℤ</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Explore</a>
