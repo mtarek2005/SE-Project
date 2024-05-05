ae44f0cdc5cb8cf0fe3335d6a9193b966fe03582
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-05 22:43:17 +0300

    post

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ require_once "./views/head.php";
     <h1>Hello, world!</h1>
     <?php
     $post = new Post;
-    $post->post_id=0;
-    $post->poster=new User;
-    $post->poster->username="goog";
-    $post->poster->display_name="Tweeter";
-    $post->poster->profile_pic="images/pexels-pixabay-45201.jpg";
-    $post->content="Some quick example text to build on the card title and make up the bulk of the card's content.";
-    $post->date=new DateTime();
-    $post->image="images/pexels-mikebirdy-170811.jpg";
-     require_once "./views/post-small.php"; 
-     ?>
+    $post->post_id = 0;
+    $post->poster = new User;
+    $post->poster->username = "goog";
+    $post->poster->display_name = "Tweeter";
+    $post->poster->profile_pic = "images/pexels-pixabay-45201.jpg";
+    $post->content = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+    $post->date = new DateTime();
+    $post->image = "images/pexels-mikebirdy-170811.jpg";
+    require_once "./views/post-small.php";
+    ?>
 </main>
 <?php
 require_once "./views/foot.php";
--- views/post-small.php
+++ views/post-small.php
@@ -3,6 +3,7 @@
         <h5 class="card-title"><img src="<?=$post->poster->profile_pic?>" class="rounded-circle pfp" alt="..."> <?=$post->poster->display_name?></h5>
         <p class="card-text tweet-username">@<?=$post->poster->username?></p>
         <p class="card-text"><?=$post->content?></p>
+        <p class="card-text tweet-date"><?=date("F j, Y, g:i a",$post->date->getTimestamp())?></p>
         <button class="btn btn-link card-link">like</button>
         <button class="btn btn-link card-link">repost</button>
         <a href="#" class="card-link">comment</a>
