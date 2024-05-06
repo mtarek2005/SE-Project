84de4e9d7d871ba4dea56bf964d65fcdd32e96f7
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-05-06 19:22:10 +0300

    color icons

--- views/post-small.php
+++ views/post-small.php
@@ -4,10 +4,10 @@
         <p class="card-text tweet-username">@<?=$post->poster->username?></p>
         <p class="card-text"><?=$post->content?></p>
         <p class="card-text tweet-date"><?=date("F j, Y, g:i a",$post->date->getTimestamp())?></p>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-heart"></i></button>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-retweet"></i></button>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-bookmark"></i></button>
-        <a href="#" class="card-link"><i class="nf nf-md-comment"></i></a>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-heart like-icon"></i></button>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-retweet rtwt-icon"></i></button>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-bookmark bkmrk-icon"></i></button>
+        <a href="#" class="card-link text-muted text-decoration-none"><i class="nf nf-md-comment cmnt-icon"></i></a>
     </div>
     <?php if (!is_null($post->image)): ?>
     <img src="<?=$post->image?>" class="card-img-bottom" alt="...">
--- views/quote-small.php
+++ views/quote-small.php
@@ -14,10 +14,10 @@
             <?php endif; ?>
         </div>
         <p class="card-text tweet-date"><?= date("F j, Y, g:i a", $post->date->getTimestamp()) ?></p>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-heart"></i></button>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-retweet"></i></button>
-        <button class="btn btn-link card-link"><i class="nf nf-fa-bookmark"></i></button>
-        <a href="#" class="card-link"><i class="nf nf-md-comment"></i></a>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-heart like-icon"></i></button>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-retweet rtwt-icon"></i></button>
+        <button class="btn btn-link card-link text-muted text-decoration-none"><i class="nf nf-fa-bookmark bkmrk-icon"></i></button>
+        <a href="#" class="card-link text-muted text-decoration-none"><i class="nf nf-md-comment cmnt-icon"></i></a>
     </div>
     <?php if (!is_null($post->image)) : ?>
         <img src="<?= $post->image ?>" class="card-img-bottom" alt="...">
